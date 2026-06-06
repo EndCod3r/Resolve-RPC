9f26c1a0e4dcac0d2df0fe2918fb0e3e9f41dac6
EndCod3r <endlord15@proton.me>
2026-06-05 18:13:55 -0800

    Fixed for real this time

--- add_env.bat
+++ add_env.bat
@@ -12,12 +12,13 @@ if not exist "%RESOLVE_PATH%\fusionscript.dll" (
 net session >nul 2>&1
 if %errorlevel% neq 0 (
     setx RESOLVE_SCRIPT_API "%%PROGRAMDATA%%\Blackmagic Design\DaVinci Resolve\Support\Developer\Scripting"
-    setx RESOLVE_SCRIPT_LIB  "C:\Program Files\Blackmagic Design\DaVinci Resolve\fusionscript.dll"
-    setx PYTHONPATH "%%PYTHONPATH%%;%%RESOLVE_SCRIPT_API%%\Modules\"
+    setx RESOLVE_SCRIPT_LIB  "%RESOLVE_PATH%\fusionscript.dll"
+    setx PYTHONPATH "%%PYTHONPATH%%;%%PROGRAMDATA%%\Blackmagic Design\DaVinci Resolve\Support\Developer\Scripting\Modules\"
+    echo User environment variables for DaVinci Resolve scripting have been added.
 ) else (
     setx /M RESOLVE_SCRIPT_API "%%PROGRAMDATA%%\Blackmagic Design\DaVinci Resolve\Support\Developer\Scripting"
-    setx /M RESOLVE_SCRIPT_LIB  "C:\Program Files\Blackmagic Design\DaVinci Resolve\fusionscript.dll"
-    setx /M PYTHONPATH "%%PYTHONPATH%%;%%RESOLVE_SCRIPT_API%%\Modules\"
+    setx /M RESOLVE_SCRIPT_LIB  "%RESOLVE_PATH%\fusionscript.dll"
+    setx /M PYTHONPATH "%%PYTHONPATH%%;%%PROGRAMDATA%%\Blackmagic Design\DaVinci Resolve\Support\Developer\Scripting\Modules\"
+    echo System environment variables for DaVinci Resolve scripting have been added.
 )
-echo Environment variables for DaVinci Resolve scripting have been added.
 pause
